11799e54b66c983ccb298481f7ae59a7a568991d
Gorkha Techie <gorkhatechie@gmail.com>
2024-12-26 00:06:02 +0545

    Home page

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@ source "https://rubygems.org"
 
 gem "jekyll", "~> 4.3.2"
 
+# group :jekyll_plugins do
+#  gem "jekyll-redirect-from"
+# end
+
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # Performance-booster for watching directories on Windows
 gem "wdm", "~> 0.1.0" if Gem.win_platform?
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,11 +3,6 @@
 $(function () {
   ("use strict"); // Start of use strict
 
-  // Activate wow.js
-  // new WOW().init();
-
-  var lightbox = $("#gallery a").simpleLightbox();
-
   // Closes responsive menu when a scroll trigger link is clicked
   $(".js-scroll-trigger").click(function () {
     $(".navbar-collapse").collapse("hide");
