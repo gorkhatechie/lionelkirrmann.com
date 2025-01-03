2509bd17e1ac8ef92b04bf6d83c88c858bb33b9c
Gorkha Techie <gorkhatechie@gmail.com>
2025-01-03 12:19:03 +0545

    Fixed animation video dialog feature

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -38,19 +38,19 @@ $(function () {
 
   // rendering youtube video in the Modal
   var $videoSrc;
-  $(".youtube-video-btn").click(function () {
+  $(".animationVideo-btn").click(function () {
     $videoSrc = $(this).data("src");
   });
   console.log($videoSrc);
   // when the modal is opened autoplay it
-  $("#youtubeVideoModal").on("shown.bs.modal", function (e) {
-    $("#youtubeVideo").attr("src", $videoSrc);
+  $("#animationVideoModal").on("shown.bs.modal", function (e) {
+    $("#animationVideo").attr("src", $videoSrc);
   });
 
   // stop playing the youtube video when I close the modal
-  $("#youtubeVideoModal").on("hide.bs.modal", function (e) {
-    $("#youtubeVideo").replaceWith(
-      '<iframe class="embed-responsive-item" src="" id="youtubeVideo" allowscriptaccess="always" allow="autoplay"></iframe>'
+  $("#animationVideoModal").on("hide.bs.modal", function (e) {
+    $("#animationVideo").replaceWith(
+      '<iframe class="embed-responsive-item" src="" id="animationVideo" allowscriptaccess="always" allow="autoplay"></iframe>'
     );
   });
 });
